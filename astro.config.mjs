845f082cddd50b8cb02a2e72c571a21b5760d51b
hsn <hsn@zh314.xyz>
2025-12-28 02:09:57 +0800

    feat: add sitemap integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://hsn8086.com",
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
-  integrations: [vue()],
+  integrations: [vue(), sitemap()],
 });
